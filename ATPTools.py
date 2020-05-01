ff78aeea6d1e2ed41a4f69f3ca38cf26aa5e9fae
Florian Humblot <florian.humblot@student.hu.nl>
2020-05-01 11:22:50 +0200

    Added typing to our lovely decorator

--- ATPTools.py
+++ ATPTools.py
@@ -1,10 +1,12 @@
 from copy import deepcopy
 
 
-def copyParameters(func):
+# copyParameters :: Callable -> Callable
+def copyParameters(func: callable) -> callable:
     """
     Decorator that copies the parameters of a function.
     """
+
     def passByValueFunction(*args):
         copied = [deepcopy(arg) for arg in args]
         return func(*copied)
